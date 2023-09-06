3caff4c2b568a897a53fc7fb9feb7e63c7718e45
Fatih YELBOĞA <fatihyelbogaaa@gmail.com>
2023-09-06 12:34:28 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>YAKINDA</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Library App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
